80f0124f7647288af836019567240dade31d5574
nuri <for9595@gmail.com>
2023-10-28 02:55:50 +0900

    config

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     username: process.env.USER_NAME,
     password: process.env.SEQUELIZE_PASSWORD,
-    database: "nodebird_book2",
+    database: "nodebird",
     host: process.env.HOST,
     dialect: process.env.DIALECT,
   },
