37ac9e3c88a1c4e0aa4a26ea6b726a9c7eebf537
nuri <for9595@gmail.com>
2023-10-28 14:56:48 +0900

    환경변수 수정2

--- config/config.js
+++ config/config.js
@@ -2,24 +2,24 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.USER_NAME,
+    username: "root",
     password: process.env.SEQUELIZE_PASSWORD,
     database: "nodebird",
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
   test: {
-    username: process.env.USER_NAME,
+    username: "root",
     password: process.env.SEQUELIZE_PASSWORD,
     database: "nodebird_test",
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
   production: {
-    username: process.env.USER_NAME,
+    username: "root",
     password: process.env.SEQUELIZE_PASSWORD,
     database: "nodebird",
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     dialect: "mysql",
     logging: false,
   },
